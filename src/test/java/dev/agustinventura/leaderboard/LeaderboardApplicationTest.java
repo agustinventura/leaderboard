7fb4e9dcb91766490aeda13ca1b897c0064b62cb
Agustín Ventura Carrasco <agustinventura@gmail.com>
2024-10-03 15:53:47 +0200

    Initial commit with documentation and minimal Spring and Cucumber dependencies

---
+++ src/test/java/dev/agustinventura/leaderboard/LeaderboardApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.agustinventura.leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LeaderboardApplicationTest {
+
+  @Test
+  public void contextLoads() {}
+
+}
